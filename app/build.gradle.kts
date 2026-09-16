2b8549eacbe65e42ae01927f52ea2de07daa2838
mehmetarslankurumsalgsb-gif <mehmetarslan.kurumsalgsb@gmail.com>
2026-09-16 09:40:28 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.exifinterface:exifinterface:1.3.7")
+    implementation("androidx.activity:activity-ktx:1.9.0")
 }
