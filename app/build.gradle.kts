fd9285c9ea46ec52f2a3f4850044c283dd2b3170
mehmetarslankurumsalgsb-gif <mehmetarslan.kurumsalgsb@gmail.com>
2026-09-16 11:38:12 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,7 @@ dependencies {
     implementation("androidx.camera:camera-camera2:1.3.4")
     implementation("androidx.camera:camera-lifecycle:1.3.4")
     implementation("androidx.camera:camera-view:1.3.4")
+
+    // Fotoğraf kırpma ekranı için
+    implementation("com.github.yalantis:ucrop:2.2.9")
 }
